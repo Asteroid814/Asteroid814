313d08b42c7be162b0cc07cc18fc9cfa13882214
Ethere4l <163339410+Ethere4l@users.noreply.github.com>
2024-03-14 02:10:49 +0530

    Delete Python-Projects-main directory

--- Python-Projects-main/band-name-generator.py
+++
@@ -1,5 +0,0 @@
-x = input("enter your city:? ")
-y = input("enter your pet's name :")
-
-print("your band name should be" + str(x) + str(y))
-# complete 
--- Python-Projects-main/name-digit-calculator.py
+++
@@ -1,2 +0,0 @@
-a = len(input("what's your name? "))  # len = length # take length  of the name from input . 
-print(a)
